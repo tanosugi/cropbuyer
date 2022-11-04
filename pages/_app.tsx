92fddfa0be1d6f8b6392b2359229f4182dd1329d
highlandscyclist <highlands.cyclist@gmail.com>
2022-11-04 12:15:55 +0000

    add drawingmanager, polygon, infowindow

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { useState } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [isLoaded, setIsLoaded] = useState(false);
+  return (
+    <>
+      {/* <Script
+        src={
+          "https://maps.googleapis.com/maps/api/js?key=" +
+          String(process.env.NEXT_PUBLIC_GOOGLE_MAP_KEY) +
+          "&v=beta&libraries=marker,places,drawing&callback=initMap"
+        }
+        strategy="beforeInteractive"
+        onLoad={() => setIsLoaded(true)}
+      />
+      {isLoaded &&  */}
+      <Component {...pageProps} />
+      // }
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,72 +1,227 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import { Grid, Image, View } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+import {
+  Autocomplete,
+  DrawingManager,
+  GoogleMap,
+  InfoWindow,
+  Polygon,
+  useLoadScript,
+} from "@react-google-maps/api";
+import exifr from "exifr";
+import { useEffect, useState } from "react";
 
-const Home: NextPage = () => {
+const libraries = ["places", "drawing", "marker"];
+const id = ["6e120bcd575d29f7"];
+
+const containerStyle = {
+  width: "100%",
+  height: "100%",
+};
+
+const polygonStrToLatLng = (polygonStr: string) => {
+  // console.log("polygonStr:", polygonStr);
+  const latAndLngArray = polygonStr.split(";");
+  // console.log("latAndLngArray:", latAndLngArray);
+  let ret: { lat: number; lng: number }[] = [];
+  latAndLngArray.forEach((item: string) => {
+    if (item == "") {
+      return;
+    }
+    const latAndLng = item.split(",");
+    // console.log("latAndLng:", latAndLng);
+    ret.push({ lat: parseFloat(latAndLng[0]), lng: parseFloat(latAndLng[1]) });
+  });
+  // console.log("ret:", ret);
+  return ret;
+};
+
+export default function Create() {
+  const [pictures, setPictures] = useState<
+    { name: string; lat: number; lng: number }[]
+  >([]);
+  const [polygons, setPolygons] = useState([
+    "25.774,-80.19;18.466,-66.118;32.321,-64.757;25.774,-80.19;",
+  ]);
+  const [coordinates, setCoordinates] = useState({ lat: 0, lng: 0 });
+  const [map, setMap] = useState();
+  const [autocomplete, setAutocomplete] =
+    useState<google.maps.places.Autocomplete>();
+  const onLoad = (autoC: google.maps.places.Autocomplete) =>
+    setAutocomplete(autoC);
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: String(process.env.NEXT_PUBLIC_GOOGLE_MAP_KEY),
+    libraries: libraries,
+    mapIds: id,
+    v: "beta",
+  });
+  const onPlaceChanged = () => {
+    // console.log("autocomplete:", autocomplete);
+    const lat = autocomplete?.getPlace()?.geometry?.location?.lat() || 0;
+    const lng = autocomplete?.getPlace()?.geometry?.location?.lng() || 0;
+    setCoordinates({ lat, lng });
+  };
+  const loadLatLngFromPicture = async () => {
+    let { latitude: lat, longitude: lng } = await exifr.gps("./rf1.jpg");
+    console.log("lat,lng:", lat, lng);
+    await setPictures([{ name: "./rf1.jpg", lat: lat, lng: lng }]);
+    await console.log("pictures:", pictures);
+  };
+  useEffect(() => {
+    // get the users current location on intial login
+    loadLatLngFromPicture();
+    navigator.geolocation.getCurrentPosition(
+      ({ coords: { latitude, longitude } }) => {
+        console.log("{ latitude, longitude }:", { latitude, longitude });
+        setCoordinates({ lat: latitude, lng: longitude });
+      }
+    );
+  }, []);
+  const onLoadDrawingManager = (drawingManager) => {
+    console.log(drawingManager);
+  };
+
+  // const onLoadPolygon = (polygon) => {
+  //   console.log("onLoadPolygon: ", polygon);
+  // };
+  const onPolygonComplete = (polygon) => {
+    console.log(polygon);
+    var coordStr = "";
+    for (var i = 0; i < polygon.getPath().getLength(); i++) {
+      coordStr += polygon.getPath().getAt(i).toUrlValue(6) + ";";
+    }
+    // console.log(coordStr);
+    setPolygons([...polygons, coordStr]);
+    // console.log("polygons:", polygons);
+  };
+  const createPolygonComponents = () => {
+    // console.log("polygons:", polygons);
+    const ret = polygons.map((item: string) => {
+      const paths = polygonStrToLatLng(item);
+      // console.log("paths:", paths);
+      return (
+        <Polygon
+          // onLoad={onLoadPolygon}
+          paths={paths}
+          options={options}
+        />
+      );
+    });
+    // console.log("createPolygonComponents ret:", ret);
+    return ret;
+  };
+  const mapOnLoad = async (mapInstance: google.maps.Map) => {
+    const buttons: [string, string, number, google.maps.ControlPosition][] = [
+      ["Rotate Left", "rotate", 20, google.maps.ControlPosition.LEFT_CENTER],
+      ["Rotate Right", "rotate", -20, google.maps.ControlPosition.RIGHT_CENTER],
+      ["Tilt Down", "tilt", 20, google.maps.ControlPosition.BOTTOM_CENTER],
+      ["Tilt Up", "tilt", -20, google.maps.ControlPosition.BOTTOM_CENTER],
+    ];
+
+    buttons.forEach(([text, mode, amount, position]) => {
+      const controlDiv = document.createElement("div");
+      const controlUI = document.createElement("button");
+
+      controlUI.classList.add("ui-button");
+      controlUI.innerText = `${text}`;
+      controlUI.addEventListener("click", () => {
+        adjustMap(mode, amount);
+      });
+      controlDiv.appendChild(controlUI);
+      mapInstance.controls[position].push(controlDiv);
+    });
+
+    const adjustMap = function (mode: string, amount: number) {
+      switch (mode) {
+        case "tilt":
+          mapInstance.setTilt(mapInstance.getTilt()! + amount);
+          break;
+        case "rotate":
+          mapInstance.setHeading(mapInstance.getHeading()! + amount);
+          break;
+        default:
+          break;
+      }
+    };
+  };
+  // const paths = [
+  //   { lat: 25.774, lng: -80.19 },
+  //   { lat: 18.466, lng: -66.118 },
+  //   { lat: 32.321, lng: -64.757 },
+  //   { lat: 25.774, lng: -80.19 },
+  // ];
+
+  const options = {
+    fillColor: "lightblue",
+    fillOpacity: 1,
+    strokeColor: "red",
+    strokeOpacity: 1,
+    strokeWeight: 2,
+    clickable: false,
+    draggable: false,
+    editable: false,
+    geodesic: false,
+    zIndex: 1,
+  };
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <meta name="description" content="Generated by create next app" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
+    isLoaded && (
+      <Grid
+        templateColumns="1fr"
+        templateRows={{ base: "repeat(2, 100vh)", large: "repeat(1, 100vh)" }}
+      >
+        <View>
+          <GoogleMap
+            onLoad={mapOnLoad}
+            mapContainerStyle={containerStyle}
+            center={coordinates}
+            // zoom={15}
+            zoom={1}
+            options={{ mapId: "caf2a1c5ecc8005e" }}
           >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
-    </div>
-  )
+            <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}>
+              <input
+                type="text"
+                placeholder="Customized your placeholder"
+                style={{
+                  boxSizing: `border-box`,
+                  border: `1px solid transparent`,
+                  width: `240px`,
+                  height: `32px`,
+                  padding: `0 12px`,
+                  borderRadius: `3px`,
+                  boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
+                  fontSize: `14px`,
+                  outline: `none`,
+                  textOverflow: `ellipses`,
+                  position: "absolute",
+                  left: "50%",
+                  marginLeft: "-120px",
+                }}
+              />
+            </Autocomplete>
+            {createPolygonComponents()}
+            <DrawingManager
+              onLoad={onLoadDrawingManager}
+              onPolygonComplete={onPolygonComplete}
+              options={{
+                drawingControl: true,
+                drawingControlOptions: {
+                  position: google.maps.ControlPosition.TOP_RIGHT,
+                  drawingModes: [google.maps.drawing.OverlayType.POLYGON],
+                },
+              }}
+            />
+            {pictures && (
+              <InfoWindow
+                position={{ lat: pictures[0].lat, lng: pictures[0].lng }}
+                options={{ maxWidth: 250 }}
+              >
+                <Image src={pictures[0].name} alt={undefined} width="20px" />
+              </InfoWindow>
+            )}
+          </GoogleMap>
+        </View>
+      </Grid>
+    )
+  );
 }
-
-export default Home
