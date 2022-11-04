2d2776b113aabb8ab03196233556fb598180a10a
highlandscyclist <highlands.cyclist@gmail.com>
2022-11-04 12:53:11 +0000

    remove comment from _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,22 +3,7 @@ import { useState } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [isLoaded, setIsLoaded] = useState(false);
-  return (
-    <>
-      {/* <Script
-        src={
-          "https://maps.googleapis.com/maps/api/js?key=" +
-          String(process.env.NEXT_PUBLIC_GOOGLE_MAP_KEY) +
-          "&v=beta&libraries=marker,places,drawing&callback=initMap"
-        }
-        strategy="beforeInteractive"
-        onLoad={() => setIsLoaded(true)}
-      />
-      {isLoaded &&  */}
-      <Component {...pageProps} />
-      // }
-    </>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
